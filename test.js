9c645fea630b55bc05944099c3c33a1efd522f67
antoine <ant.hess2@gmail.com>
2017-01-20 10:55:45 +0100

    Premier test mocha

---
+++ test.js
@@ -0,0 +1,8 @@
+var assert = require('chai').assert;
+let test = false;
+
+describe('leTEST', function() {
+	it('On espère que la valeur de test est bien vrai !?', function() {
+		assert.equal(test,true, 'La valeur vrai n\'est pas VRAI !');
+	});
+});
