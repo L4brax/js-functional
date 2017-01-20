157f29277469c9f2869d07843cf3aa35877c5008
antoine <ant.hess2@gmail.com>
2017-01-20 14:11:36 +0100

    Deuxieme test mocha de la fonction de calcul du panier.

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
-  });
-});
+let prices = {banana:1, potato:2, tomato:3, cucumber:4, salad:5, apple:6,
+				getBanana: function(){return this.banana},
+				getPotato: function(){return this.potato},
+				getTomato: function(){return this.tomato},
+				getCucumber: function(){return this.cucumber},
+				getSalad: function(){return this.salad},
+				getApple: function(){return this.apple}
+				}
 
+let products = ['tomato', 'cucumber', 'salad', 'cucumber', 'tomato', 'salad', 'tomato', 'cucumber',
+				'salad', 'cucumber', 'banana', 'banana']
+
+function computePrice(basket){
+	let price = 0
+	basket.forEach(function(product){
+		price += prices[product]
+	})
+	return price
+	// let price
+	// price = basket.map((product)=> price + prices[product])
+	// console.log(price)
+}
+
+console.log(computePrice(products))
+
+module.exports = {
+	computePrice : computePrice
+}
--- test.js
+++ test.js
@@ -1,8 +1,29 @@
-var assert = require('chai').assert;
-let test = false;
+var assert = require('chai').assert
+var computePrice = require('index.js').computePrice
 
-describe('leTEST', function() {
-	it('On espère que la valeur de test est bien vrai !?', function() {
-		assert.equal(test,true, 'La valeur vrai n\'est pas VRAI !');
-	});
-});
+//Jeu de donnée
+let prices = {banana:1, potato:2, tomato:3, cucumber:4, salad:5, apple:6,
+				getBanana: function(){return this.banana},
+				getPotato: function(){return this.potato},
+				getTomato: function(){return this.tomato},
+				getCucumber: function(){return this.cucumber},
+				getSalad: function(){return this.salad},
+				getApple: function(){return this.apple}
+				}
+
+let products = ['tomato', 'cucumber', 'salad', 'cucumber', 'tomato', 'salad', 'tomato', 'cucumber',
+				'salad', 'cucumber', 'banana', 'banana']
+
+
+//Les TESTS
+describe('computePrice', function() {
+	it('Empty basket should return 0 for each value', function(){
+		expect(computePrice([])).to.be.a('Number')
+		expect(computePrice([])).to.equal(0)
+	})
+	it('Basket with only one product should return the price of the product', function(){
+		expect(computePrice(['tomato'])).to.be.a('Number')
+		expect(computePrice(['tomato'])).to.equal(3)
+	})
+})
+
